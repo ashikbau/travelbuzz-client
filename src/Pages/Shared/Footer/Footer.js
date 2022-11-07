e3478d9777821284c7e9daacf7a2fe68ed7a4412
Your Name <you@example.com>
2022-11-07 19:27:43 +0100

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,9 +2,32 @@ import React from 'react';
 
 const Footer = () => {
     return (
+        <footer className="footer p-32 bg-black text-white">
         <div>
-            <h2>This is from footer</h2>
+            <img  alt="" />
+            <p>Travelbuzz a self-employed singel person owned company<br/>Providing reliable Tourisms Service from 2017</p>
         </div>
+        <div>
+            <span className="footer-title">Services</span>
+            <a href="/"className="link link-hover">Tour to Meseum</a>
+            <a href="/"className="link link-hover">Tour To Safarry Park</a>
+            <a href="/"className="link link-hover">Arrange Foods</a>
+            <a href="/"className="link link-hover">Provides Transport</a>
+        </div>
+        <div>
+            <span className="footer-title">About My Service</span>
+            <a href="/"className="link link-hover">About Me</a>
+            <a href="/"className="link link-hover">Contact</a>
+            <a href="/"className="link link-hover">Jobs</a>
+            <a href="/"className="link link-hover">Press kit</a>
+        </div>
+        <div>
+            <span className="footer-title">Legal</span>
+            <a href="/"className="link link-hover">Terms of use</a>
+            <a href="/"className="link link-hover">Privacy policy</a>
+            <a href="/"className="link link-hover">Cookie policy</a>
+        </div>
+    </footer>
     );
 };
 
