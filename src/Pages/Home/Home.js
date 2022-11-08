5a8c2fd749c09a003f475ccb7aef2dae5b78e3e6
Your Name <you@example.com>
2022-11-08 12:58:07 +0100

    added service in home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+
+  const services = useLoaderData();
+  console.log(services)
+ 
     return (
   <div >
  
-    <div className="hero min-h-screen bg-base-200 ">
+    <div className="hero bg-base-200 ">
   <div className="hero-content flex-col lg:flex-row-reverse ">
-    <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
+    <img alt='' src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">I am Looking Looking forward to <br />finally meet you</h1>
       <p className="py-6">I am excited to be guide you during your travel. I  have been working as a tour guide for the past five years. I love sharing my knowledge of history with others and making history come alive for visitors.</p>
 
       <div>
     <h4 className='text-2xl'>My Services</h4>
+    {
+      services.map(service => <p key="service_id">{service.service}</p>)
+    }
   </div>
       <button className="btn btn-primary">See All</button>
     </div>
   </div>
+  
 </div>
   </div>
     );
